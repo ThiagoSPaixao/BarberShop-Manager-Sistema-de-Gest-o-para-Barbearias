772bc58c126d8c94680c355533366630c9ad4ae2
ThiaguinhoxD <thiaguinhoescolhido26@gmail.com>
2025-12-05 19:11:40 -0300

    criando lista de produtos e serviços

--- main.py
+++ main.py
@@ -33,6 +33,25 @@ frame_direita.pack(side="right", fill="both", expand=True, padx=10, pady=10)
 label_servicos = ctk.CTkLabel(frame_direita, text="SERVIÇOS", font=("Arial", 18, "bold"))
 label_servicos.pack(anchor="nw", pady=(10, 5), padx=10)
 
+# 10. Lista estática de serviços (temporária)
+lista_servicos = ctk.CTkFrame(frame_direita, corner_radius=8)
+lista_servicos.pack(fill="x", padx=15, pady=5)
+
+ctk.CTkLabel(lista_servicos, text="Corte Tesoura __________ R$ 22,00").pack(anchor="w", pady=2)
+ctk.CTkLabel(lista_servicos, text="Corte Máquina  _________ R$ 20,00").pack(anchor="w", pady=2)
+ctk.CTkLabel(lista_servicos, text="Máquina + Tesoura  ____ R$ 24,00").pack(anchor="w", pady=2)
+
+# 11. Título PRODUTOS
+label_produtos = ctk.CTkLabel(frame_direita, text="PRODUTOS", font=("Arial", 18, "bold"))
+label_produtos.pack(anchor="nw", pady=(20, 5), padx=10)
+
+# 12. Lista estática de produtos (temporária)
+lista_produtos = ctk.CTkFrame(frame_direita, corner_radius=8)
+lista_produtos.pack(fill="x", padx=15, pady=5)
+
+ctk.CTkLabel(lista_produtos, text="Gel Capilar _____________ R$ 8,00").pack(anchor="w", pady=2)
+ctk.CTkLabel(lista_produtos, text="Pomada Modeladora _______ R$ 25,00").pack(anchor="w", pady=2)
+ctk.CTkLabel(lista_produtos, text="Espuma de Barbear _______ R$ 15,00").pack(anchor="w", pady=2)
 
 
 #Iniciar o loop da interface
